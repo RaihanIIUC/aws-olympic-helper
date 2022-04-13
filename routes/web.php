c3bf6ceb6dc318afa42092bea620c593cfa014a0
RaihanIIUC <mdraihan4835@gmail.com>
2022-04-13 14:32:07 +0600

    add up

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/hello', [SentSmsController::class, 'hello']);
+Route::get('/', [SentSmsController::class, 'hello']);
+
+// Route::get('/hello', [SentSmsController::class, 'hello']);
