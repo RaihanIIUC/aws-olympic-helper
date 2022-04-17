85a6fb05dda70123ed2397108313c0665e39ee86
RaihanIIUC <mdraihan4835@gmail.com>
2022-04-17 12:59:02 +0600

    fix 5

--- app/Http/Controllers/BdappsController.php
+++ app/Http/Controllers/BdappsController.php
@@ -3,71 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Classes\UssdReceiver;
-use App\Classes\UssdSender;
-use App\Classes\UssdException;
-use App\Classes\Subscription;
-use App\Classes\SubscriptionException;
-
 use App\Classes\SMSSender;
 use App\Classes\SMSReceiver;
 use App\Classes\SMSServiceException;
-
-use App\Classes\SubscriptionReceiver;
-
-use App\Models\item;
-use App\Models\route;
-use App\Models\error_message;
-use App\Models\download_message;
-use App\Models\voucher_details;
-use App\Models\voucher_head;
-use Session;
-use App\Models\route_with_number;
 use App\Models\response_log;
 use App\Models\SentSms;
-use DB;
-use Illuminate\Support\Facades\DB as FacadesDB;
+
 
 class BdappsController extends Controller
 {
 
-
-
-    // sms response reciver function to store the response and data field that we 
-    // can need to store for olympic aws system
+    /**
+     *  sms response reciver function to store the response and data field that we 
+     * can need to store for olympic aws system
+     */
     public function sms(Request $request)
     {
 
         $appid = "APP_060322";
         $apppassword = "edf54eb9915fb5064caea8778368dd9c";
-
+        $SendingSmsBaseUrl = "https://developer.bdapps.com/sms/send";
         try {
-
-
             // Creating a receiver and intialze it with the incomming data
             $receiver = new SMSReceiver($request->all());
+            $sender = new SmsSender($SendingSmsBaseUrl, $appid, $apppassword);
 
-
-            $sender = new SmsSender("https://developer.bdapps.com/sms/send", $appid, $apppassword);
-
-            //Creating a sender
-
+            //Creating reciver instance with its dependencies
             $message = $receiver->getMessage(); // Get the message sent to the app
             $address = $receiver->getAddress();    // Get the phone no from which the message was sent 
-            $jsonInfo = $receiver->getJson();
 
+            // we try here sms instead of broadcasting but it fails to send data to the server
+            // to server , so then we uses the broadcast function to make it works
+            $smsSendingToUser = $sender->broadcast('Thank you for your SMS' . '  ' . $message);
 
-            //---------- 	Send a SMS to a particular user
-            // $response = $sender->sms('Thanks for your response', $address);
-            $response2 = $sender->broadcast('Thank you for your SMS' . '  ' . $message);
-
-
-
-            if ($response2->statusCode == 'S1000') {
+            // a constrains to keep the status( boolean ) up to date , if 
+            // the sms is successfully sent to user with customized message
+            // then the status is 1 , if any way failed = -1
+            if ($smsSendingToUser->statusCode == 'S1000') {
                 $status = 1;
             } else {
                 $status = -1;
             }
+
 
             // storing the api calls request params in database.
             SentSms::create([
@@ -77,31 +54,35 @@ class BdappsController extends Controller
                 'requestId' => $request->requestId
             ]);
 
+
             // storing the data in json format as response log
             response_log::create([
                 'applicationId' =>
                 $request->applicationId,
                 'status' => $status,
-                'response' => $response2
+                'response' => $smsSendingToUser
             ]);
 
 
-
-            // return $response;
+            // return failed responses $response;
         } catch (SMSServiceException $e) {
             $response2 = $sender->broadcast('you sms sent failed' . '  ' . $message);
         }
     }
 
-
-
+    /**
+     * A function which search data by two date , and pull it to olympic 2 solution 
+     * it was checked out and hope it will works fine.
+     */
     public function SearchByDate(Request $request)
     {
         $start_date = date('Y-m-d', strtotime($request->start_at));
         $end_date = date('Y-m-d', strtotime($request->end_at));
 
+        // this actually a orm calling where we have to give 2 params [starting date , last date ] 
+        // which times data we want to full it from the database to keep the olympic system
+        // up to date with the user subscription_status
         $queryByDate = SentSms::whereBetween('created_at', [$start_date, $end_date])->get();
-
         return response()->json([$queryByDate], 200);
     }
 }
