64a566a218719baf182a3f1149591740f7573a3c
RaihanIIUC <mdraihan4835@gmail.com>
2022-04-25 11:09:07 +0600

    fix 17

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,11 +153,13 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
+
+
+                    @include('helper.range')
+
                     <div class="card-body text-center">
                         <h5 class="card-title m-b-0">Table In our hand {{ $total }}</h5>
                     </div>
-
-                    @include('helper.range')
 
                     <div class="table-responsive">
                         <table class="table">
