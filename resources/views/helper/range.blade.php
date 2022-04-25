815641795a353379ac11220b655476aa0f3bf4e0
RaihanIIUC <mdraihan4835@gmail.com>
2022-04-25 10:57:15 +0600

    fix 28

--- resources/views/helper/range.blade.php
+++ resources/views/helper/range.blade.php
@@ -1,30 +1,23 @@
 <div class="col-lg-12">
-    <h2 class=" text-center">(Date Range) To Pull data </h2>
+    <h2 class="mb-5 text-center">(Date Range) To Pull data </h2>
     <form action="{{ route('query')}}" method="POST" class="row">
         @csrf
 
-        <div class="col-md-5">
+        <div class="col-md-6">
             <div class="form-group">
                 <label for="input_from">From</label>
                 <input type="date" class="form-control" name="start" id="input_from" placeholder="Start Date">
             </div>
         </div>
-        <div class="col-md-5">
+        <div class="col-md-6">
             <div class="form-group">
                 <label for="input_to">To</label>
                 <input type="date" class="form-control" name="end" id="input_to" placeholder="End Date">
             </div>
         </div>
 
-        <div class="col-md-3">
-            <div class="form-group">
-                <label for="input_to">To</label>
-                <button type="submit" class="btn btn-success">Download</button>
-            </div>
-        </div>
-        <!-- 
         <div class="col-md-12  d-flex justify-content-center">
             <button type="submit" class="btn btn-success">Download</button>
-        </div> -->
+        </div>
     </form>
 </div>
