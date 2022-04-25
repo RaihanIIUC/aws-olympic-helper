e146ab691d16e769e12fa1ad3f7c7a01c8cef056
RaihanIIUC <mdraihan4835@gmail.com>
2022-04-25 10:54:10 +0600

    fix 31

--- resources/views/helper/range.blade.php
+++ resources/views/helper/range.blade.php
@@ -3,20 +3,20 @@
     <form action="{{ route('query')}}" method="POST" class="row">
         @csrf
 
-        <div class="col-md-6">
+        <div class="col-md-5">
             <div class="form-group">
                 <label for="input_from">From</label>
                 <input type="date" class="form-control" name="start" id="input_from" placeholder="Start Date">
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-5">
             <div class="form-group">
                 <label for="input_to">To</label>
                 <input type="date" class="form-control" name="end" id="input_to" placeholder="End Date">
             </div>
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-3">
             <div class="form-group">
                 <label for="input_to">To</label>
                 <button type="submit" class="btn btn-success">Download</button>
