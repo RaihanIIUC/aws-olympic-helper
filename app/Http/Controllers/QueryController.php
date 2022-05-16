d4ef63b068cd1706fc32223dc3fe48eaeae640ef
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-05-16 14:31:49 +0600

    Update QueryController.php

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -14,9 +14,15 @@ class QueryController extends Controller
     {
         $start_date = date('Y-m-d', strtotime($request->start_at));
         $end_date = date('Y-m-d', strtotime($request->end_at));
+        $foundSms = SentSms::whereDate('created_at','>=',$start_date)->whereDate('created_at','<=',$end_date)->get();
 
-        $foundSms = SentSms::whereBetween('created_at', [$start_date, $end_date])->paginate(5);
 
-        return view('query', compact('foundSms'));
+        // $foundSms = SentSms::whereBetween('created_at', [$start_date, $end_date])->get();
+    
+
+        
+        $no_count = 1;
+
+        return view('query', compact('foundSms','no_count'));
     }
 }
