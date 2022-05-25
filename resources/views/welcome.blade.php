1328d6cebd79724d1c9ae56801ac50ec224e0bf0
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-05-25 17:23:49 +0600

    fix 8 major fix it looks we can go now production level

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,56 +148,55 @@
 </head>
 
 <body>
-    @livewire('post-form')
 
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
 
 
-                        @include('helper.range')
+                    @include('helper.range')
 
-                        <div class="card-body text-center">
-                            <h5 class="card-title m-b-0">Table In our hand {{ $total }}</h5>
-                        </div>
+                    <div class="card-body text-center">
+                        <h5 class="card-title m-b-0">Table In our hand {{ $total }}</h5>
+                    </div>
 
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead class="thead-light">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">SL</th>
+                                    <th scope="col">Message</th>
+                                    <th scope="col">sourceAddress</th>
+                                    <th scope="col">Created at</th>
+
+
+                                </tr>
+                            </thead>
+                            <tbody class="customtable">
+                                @foreach($sms as $s)
                                     <tr>
-                                        <th scope="col">SL</th>
-                                        <th scope="col">Message</th>
-                                        <th scope="col">sourceAddress</th>
-                                        <th scope="col">Created at</th>
-
-
+                                        <td>{{ $no_count++ }}</td>
+                                        <td>{{ $s->message }}</td>
+                                        <td>{{ $s->sourceAddress }}</td>
+                                        <td>{{ $s->created_at }}</td>
                                     </tr>
-                                </thead>
-                                <tbody class="customtable">
-                                    @foreach($sms as $s)
-                                        <tr>
-                                            <td>{{ $no_count++ }}</td>
-                                            <td>{{ $s->message }}</td>
-                                            <td>{{ $s->sourceAddress }}</td>
-                                            <td>{{ $s->created_at }}</td>
-                                        </tr>
-                                    @endforeach
+                                @endforeach
 
-                                </tbody>
-                            </table>
+                            </tbody>
+                        </table>
 
-                            <div class="d-flex">
-                                <div class="mx-auto">
-                                    {{ $sms->links("pagination::bootstrap-4") }}
-                                </div>
+                        <div class="d-flex">
+                            <div class="mx-auto">
+                                {{ $sms->links("pagination::bootstrap-4") }}
                             </div>
-
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </body>
 
 </html>
