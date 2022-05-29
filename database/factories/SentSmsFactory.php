758f977f9243b52fe6310ad009fbe4dffc56ab8e
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-05-29 18:22:33 +0600

    Update SentSmsFactory.php

--- database/factories/SentSmsFactory.php
+++ database/factories/SentSmsFactory.php
@@ -21,7 +21,7 @@ class SentSmsFactory extends Factory
             'sourceAddress' =>  'APP-9864752',
             'message' =>   'Et labore nostrum repellat tenetur facilis in quisquam. Ea et explicabo enim eveniet aut explicabo similique nulla.',
             'requestId' =>  '304.330.5912',
-            // 'created_at' => $this->faker->dateTimeThisYear('+2 months')
+            'created_at' => $this->faker->dateTimeThisYear('+2 months')
         ];
     }
 }
