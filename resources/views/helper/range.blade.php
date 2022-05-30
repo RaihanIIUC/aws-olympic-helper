6faa0cba7e83c83afa8583a5be674129c63379b9
RaihanIIUC <mdraihan4835@gmail.com>
2022-05-30 13:54:41 +0600

    fix 2w2

--- resources/views/helper/range.blade.php
+++ resources/views/helper/range.blade.php
@@ -1,18 +1,18 @@
 <div class="col-lg-12 border border-dark my-4">
     <h2 class="mb-5 text-center">(Date Range) To Pull data </h2>
-    <form action="{{ route('query')}}" method="POST" class="row">
+    <form action="{{ route('query') }}" method="POST" class="row">
         @csrf
 
         <div class="col-md-6">
             <div class="form-group">
                 <label for="input_from">From</label>
-                <input type="date" class="form-control" name="start" id="input_from" placeholder="Start Date">
+                <input type="date" class="form-control" name="start_at" id="input_from" placeholder="Start Date">
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label for="input_to">To</label>
-                <input type="date" class="form-control" name="end" id="input_to" placeholder="End Date">
+                <input type="date" class="form-control" name="end_at" id="input_to" placeholder="End Date">
             </div>
         </div>
 
