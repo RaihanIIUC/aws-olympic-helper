889476a6227a58c50e81b70daebc125ddfd4e5bb
RaihanIIUC <mdraihan4835@gmail.com>
2022-06-08 12:34:34 +0600

    fix

--- database/factories/SentSmsFactory.php
+++ database/factories/SentSmsFactory.php
@@ -22,7 +22,7 @@ class SentSmsFactory extends Factory
             'sourceAddress' =>  'APP-9864752',
             'message' =>   'Et labore nostrum repellat tenetur facilis in quisquam. Ea et explicabo enim eveniet aut explicabo similique nulla.',
             'requestId' =>  '304.330.5912',
-            'created_at' => $this->faker->dateTimeBetween('+0 days', '-2 days')
+            'created_at' => $this->faker->dateTimeBetween('-2 days', '+0 days')
             //   'created_at' => new Carbon('2022-05-30')
 
         ];
