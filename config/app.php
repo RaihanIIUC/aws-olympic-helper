e89b3199be1b135aaaa961f98d4d314fb95819bf
RaihanIIUC <mdraihan4835@gmail.com>
2022-06-26 21:23:37 +0600

    fix 6

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        'Barryvdh\Debugbar\ServiceProvider',
 
         /*
          * Package Service Providers...
@@ -193,8 +193,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
-
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
 
     ])->toArray(),
 
